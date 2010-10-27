71a96fa6c42c246408264d3d5ca1d75166d7dde2
cbeck <chris@netphase.com>
2010-10-27 12:01:48 -0400

    placeholder pages for content

---
+++ web/partner.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>LocationMonitor.com - Partner programs</title>
+  <?php include("doc_head.inc"); ?>
+</head>
+<body>
+  <div id="canvas" class="container_12">
+    <?php include("header.inc"); ?>
+    <div id="main" class="container_12">
+      <h2>Partner programs</h2>
+    </div>
+    <?php include("footer.inc"); ?>  
+  </div>
+</body>
+</html>
