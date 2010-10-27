875cc42d736f6001cd16c8d156f492734b49ebb7
cbeck <chris@netphase.com>
2010-10-27 11:27:42 -0400

    turning into php includes for common stuff

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title>LocationMonitor.com</title>
+  <?php include("doc_head.inc"); ?>
+</head>
+<body>
+  <div id="canvas" class="container_12">
+    <?php include("header.inc"); ?>
+    <div id="splash" class="container_12">
+      <div class="grid_6">
+        <img src="images/what-are-they-saying.png"/>
+        <div class="center"><a href="#"><img src="images/btn-find-out.png"/></a></div>
+      </div>
+      <div class="grid_6"></div>
+    </div>
+    <div id="main" class="container_12">
+      <div class="content_box"></div>
+      <div class="content_box"></div>
+      <div class="content_box"></div>
+    </div>
+    <?php include("footer.inc"); ?>  
+  </div>
+</body>
+</html>
