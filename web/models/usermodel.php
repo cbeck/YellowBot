818ff0ccbb82b4590dadc34e07de72b874322805
JCBarry <jay@jcbarry.com>
2010-10-31 22:43:50 -0400

    Created user model.

--- web/models/usermodel.php
+++ web/models/usermodel.php
@@ -4,9 +4,37 @@ class UserModel extends Model {
 		parent::Model();
 	}
 	
+	function get_user_by_id($id) {
+		$query = $this->db->get_where('user', array('id' => $id), 1);
+		return $query->result();
+	}
+	
+	function get_user_by_email($email) {
+		$query = $this->db->get_where('user', array('email' => $email), 1);
+		return $query->result();
+	}
+	
 	function get_users() {
 		$query = $this->db->get('user');
 		return $query->result();
 	}
+	
+	function add_user($data) {
+		$this->db->insert('user', $data);
+	}
+	
+	function update_user($id, $data) {
+		$this->db->where('id',$id);
+		$this->db->update('user', $data);
+	}
+	
+	function delete_user($id) {
+		$this->db->where('id', $id);
+		$this->db->delete('user');
+	}
+
+//Protected API Calls...
+
+
 }
 ?>
