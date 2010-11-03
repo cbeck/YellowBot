1e47e0817e355c0bc77604eca0d0ba92555b211e
JCBarry <jay@jcbarry.com>
2010-11-02 22:46:11 -0400

    Added API key and Secret to application config.

---
+++ web/config/yellowbot.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//Yellowbot API Key
+$config['api_key'] = "59667cf953f4c37f0d760c003a287972";
+//Yellowbot API Secret
+$config['api_secret'] = "f89d48e031440891";
