d2e0ee6cd7676a712a3bba587a8a6f2beabab8c5
cbeck <chris@netphase.com>
2010-11-05 15:00:25 -0400

    new text per CT

--- web/views/static/faq.php
+++ web/views/static/faq.php
@@ -8,6 +8,28 @@
     <?php include(dirname(__FILE__)."/../includes/header.inc"); ?>
     <div id="main" class="container_12">
       <h2>FAQs</h2>
+      <dl>
+        <dt>How is Reputation Monitoring defined by LocationMonitor.com?</dt>
+        <dd>We are monitoring the reputation and visibility of businesses with a specific focus on locations. We track the visibility and accuracy of the business location’s presence as it is seen on search engines, online yellow pages and social sites, displaying information that may need to be updated. We also track reviews, mentions and ratings for that business, as well as a scoring system against their competitors.</dd>
+        <dt>What if I have multiple locations?</dt>
+        <dd>Each location should have its own account, as we try to align the monitoring to specific branches, restaurants, bars, offices, surgeries etc.</dd>
+        <dt>Can I use this to track personnel or specific brands?</dt>
+        <dd>This not designed today for specific people or products, but results will show up if they are referenced in relation to a business at a location.</dd> 
+        <dt>Is there an annual subscription minimum?</dt>
+        <dd>No, we bill quarterly in advance and you may cancel any time and remove the tracking from our system.</dd>
+        <dt>How quickly will reporting happen?</dt>
+        <dd>Some results are almost immediate, but our spidering technology improves with time, so it will begin to be most comprehensive after two weeks.</dd>
+        <dt>Will I see results from specialist sites from my industry?</dt>
+        <dd>Yes, the results are targeted as closely as possible, so if you are a restaurant you will see reviews from dining sites, and if you are a hotel you will see more results from travel vertical sites.</dd>
+        <dt>Can I see a list of the sites you monitor?</dt>
+        <dd>We monitor hundreds of sites, and the list changes constantly. If you feel we are overlooking a site that should be tracked, you may send emails to <a href="mailto:contact@locationmonitor.com">contact@locationmonitor.com</a>.</dd>
+        <dt>How do you determine my competitors?</dt>
+        <dd>This is done by vertical in the adjacent geography, with a focus on businesses with the most activity. This means you get to see the reputation score for the competitors to whom you most likely need to be paying most attention.</dd>
+        <dt>Can I respond to reviews from the site?</dt>
+        <dd>A link is provided to the location of the review. Although there may be some methods for "remote posting" of comments, we believe the best practice is for a business to click through to the actual page to see the context of the review and look for any latest comments so the best response can be crafted.</dd>
+        <dt>Can I edit my listings from the site?</dt>
+        <dd>We provide direct links to the listings on the sites. Some of these you may need to log into in order to claim or edit the listings. Of course we recommend any listing activity is synchronized with our sister site UniversalBusinessLising.org (<a href="http://www.UBL.org">www.UBL.org</a>).</dd>
+      </dl>
     </div>
     <?php include(dirname(__FILE__)."/../includes/footer.inc"); ?>  
   </div>
--- web/views/static/partner.php
+++ web/views/static/partner.php
@@ -7,7 +7,15 @@
   <div id="canvas" class="container_12">
     <?php include(dirname(__FILE__)."/../includes/header.inc"); ?>
     <div id="main" class="container_12">
-      <h2>Partner programs</h2>
+      <h2>Partner Programs</h2>
+      <p>Partner With LocationMonitor.com</p>
+      <p>Earn monthly recurring revenue with every sign-up.</p>
+      <h4>Affiliates</h4>
+      <p>Become an Affiliate and receive commissions every month for referral signups. Take advantage of your customers' concern about their online reputation, their reviews, their ratings, mentions and competition.</p>
+      <p>Upon approval, you will receive an affiliate code, custom links and images you can place on your site to promote the service via your Website, email or other means. Affiliates will receive a percentage of the monthly fees from the monitoring service. This is an ideal service for Affiliates with B2B users and sites that help businesses with their Internet identities, directory listings, search engine marketing and Web presence. Payouts are made monthly as long as certain minimums are met.</p>
+      <p>Join our program today by registering at our sister site's Affiliate page >> <a href="http://www.affiliate.UBL.org">www.affiliate.UBL.org</a></p>
+      <h4>Channel Partners and Agencies</h4>
+      <p>LocationMonitor.com is available for revenue-sharing with resellers in a variety of ways. In addition to the Affiliate link method above, the service is offered via Private Label and API methods. For information, please send information about your interest to <a href="mailto:contact@locationmonitor.com">contact@locationmonitor.com</a>.</p>
     </div>
     <?php include(dirname(__FILE__)."/../includes/footer.inc"); ?>  
   </div>
