5a94ac71a516cdbb458034f406876de4c53980ef
cbeck <chris@netphase.com>
2010-11-05 16:10:58 -0400

    adding jquery ui for interface goodness and beginnings of features page

--- web/views/static/features.php
+++ web/views/static/features.php
@@ -8,6 +8,29 @@
     <?php include(dirname(__FILE__)."/../includes/header.inc"); ?>
     <div id="main" class="container_12">
       <h2>Features</h2>
+      <h5>Overview</h5>
+      <p>Get an executive summary of your web presence and reputation. Use this page as an entry point to quickly navigate to other sections.</p>
+      <h5>Visibility</h5>
+      <p>There's a lot of bad data out there. Manage your web presence and enhance your search engine optimization by finding and correcting inaccurate listings across the web.</p>
+      <h5>Reviews</h5>
+      <p>Save time by viewing reviews from all across the web. Plus, you get the tools to share and respond to those reviews – all in one place.</p>
+      <h5>Mentions</h5>
+      <p>Reviews are not the only way people talk about a business - there is also tweeting, sharing photos, and "checking in". Manage information overload with a single page to quickly see everything that is being said about your business.</p>
+      <h5>Competitors</h5>
+      <p>Put reviews and mentions into perspective: see how your online reputation compares to your  direct competitors.</p>
+      
+      <h3>What are the benefits of Reputation and Presence Management System?</h3>
+      <ul>
+        <li><p>Alert/Be alerted to negative comments so you can take immediate action and respond.</p></li>
+        <li><p>Protect/Protect your Brand and company name from misuse or abuse.</p></li>
+        <li><p>Connect/Make sure customers are able to connect with accurate phone and address information in listings.</p></li>
+        <li><p>Compete/ See how the sentiment about your business compares with your competitors.</p></li>
+      </ul>
+      
+      <h3>What is the Reputation and Presence Management?</h3>
+      <p>LocationMonitor.com helps you monitor your online reputation by finding everything that is said about your business in reviews and on social networking sites and displaying it in one easy-to-use dashboard.</p>
+      <p>There's also a lot of bad local information on the web -- incomplete, inaccurate, or duplicative listings that can make it difficult for you to connect with your customers. LocationMonitor.com's reputation management system finds that listing information and makes it easy for you to correct it.</p>
+      <p>Locationmonitor.com provides a single location where you can not only understand what's being said about you online, but also actively manage your information and participate in conversations that are happening across the Web. Don't let the buzz turn into a buzz-saw!</p>
     </div>
     <?php include(dirname(__FILE__)."/../includes/footer.inc"); ?>  
   </div>
