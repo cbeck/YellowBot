2e4567fd7d196a3febf45d7a839a6a7410883370
JCBarry <jay@jcbarry.com>
2010-11-04 21:44:45 -0400

    Refactored and completed yellowbot model.

--- web/models/yellowbotmodel.php
+++ web/models/yellowbotmodel.php
@@ -20,21 +20,68 @@ class YellowbotModel extends Model {
     $operation = "/api/reputation_management/register_user?";
     $query = array(
       "api_key" => $this->api_key,
-      "name" => str_replace (" ", "", $name),
+      "name" => $name,
       "email" => $email,
       "api_ts" => time(),
       "api_user_identifier" => md5($email));
      
     $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
-    curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
-    return $this->response_parse($response);
+    return $this->curl_request($query_string, $operation);
   }
   
-  function  repman_search_location($zip, $phone) {
+  ///api/reputation_management/register_location
+  function repman_register_location($data) {
+    $operation = "/api/reputation_management/register_location?";
+    $query = array(
+      "api_key" => $this->api_key,      
+      "name" => $data['name'],
+      "address" => $data['address1'].' '.$data['address2'],
+      "city_name" => $data['city_name'],
+      "state" => $data['state'],
+      "zip" => $data['zip'],
+      "phone_number" => $data['phone_number'],
+      "email" => $data['email'],
+      "api_ts" => time(),
+      "api_user_identifier" => md5($data['email']));
+     
+    $query_string = $this->generate_query($query).$this->generate_sig($query); 
+    return $this->curl_request($query_string, $operation);    
+  }
+  
+  ///api/reputation_management/update_location
+  function repman_update_location($data, $location_id) {
+    $operation = "/api/reputation_management/update_location?";
+    $query = array(
+      "api_key" => $this->api_key,
+      "location_id" => $location_id,      
+      "name" => $data['name'],
+      "address" => $data['address1'].' '.$data['address2'],
+      "city_name" => $data['city_name'],
+      "state" => $data['state'],
+      "zip" => $data['zip'],
+      "phone_number" => $data['phone_number'],
+      "email" => $data['email'],
+      "api_ts" => time(),
+      "api_user_identifier" => md5($data['email']));
+     
+    $query_string = $this->generate_query($query).$this->generate_sig($query); 
+    return $this->curl_request($query_string, $operation);   
+  }  
+  
+  ///api/reputation_management/unregister_location
+  function repman_unregister_location($location_id) {
+    $operation = "/api/search/locations?";
+    $query = array(
+      "api_key" => $this->api_key,
+      "location_id" => $location_id,
+      "api_ts" => time(),
+      "api_user_identifier" => md5($email));
+    
+    $query_string = $this->generate_query($query).$this->generate_sig($query); 
+    return $this->curl_request($query_string, $operation);    
+  }
+  
+  function  repman_search_locations($zip, $phone) {
     $operation = "/api/search/locations?";
     $query = array(
       "api_key" => $this->api_key,
@@ -42,12 +89,7 @@ class YellowbotModel extends Model {
       "q" => $phone);
      
     $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
-    curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
-    return $this->response_parse($response);    
+    return $this->curl_request($query_string, $operation);    
   }
   
   function repman_list_locations($email) {
@@ -58,12 +100,7 @@ class YellowbotModel extends Model {
       "api_user_identifier" => md5($email));
      
     $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
-    curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
-    return $this->response_parse($response); 
+    return $this->curl_request($query_string, $operation); 
     
   }
   
@@ -77,95 +114,20 @@ class YellowbotModel extends Model {
     $query_string = $this->generate_query($query).$this->generate_sig($query); 
     $url = $this->base_url.$operation.$query_string;
     redirect($url);    
-  }
+  }   
   
-  ///api/reputation_management/register_location
-  function repman_register_location($data) {
-    $data = $this->encode_array($data);
-    $operation = "/api/reputation_management/register_location?";
-    $query = array(
-      "api_key" => $this->api_key,      
-      "name" => $data['name'],
-      "address" => $data['address1'].' '.$data['address2'],
-      "city_name" => $data['city_name'],
-      "state" => $data['state'],
-      "zip" => $data['zip'],
-      "country" => "USA",
-      "phone_number" => $data['phone_number'],
-      "fax_number" => $data['fax_number'],
-      "tollfree_number" => $data['tollfree_number'],
-      "payment" => "",
-      "hours_open" => "",
-      "email" => urlencode($data['email']),
-      "website" => $data['website'],
-      "api_ts" => time(),
-      "api_user_identifier" => md5($data['email']));
-     
-    $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
+  //Protected utility functions
+  protected function curl_request($query, $operation) {    
+    $curl_url = $this->base_url.$operation.$query;    
     curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
-    return $this->response_parse($response);    
-  }
-
-  ///api/reputation_management/update_location
-  function repman_update_location($data, $location_id) {
-    $data = $this->encode_array($data);
-    $operation = "/api/reputation_management/update_location?";
-    $query = array(
-      "api_key" => $this->api_key,
-      "location_id" => $location_id,
-      "name" => $data['name'],
-      "address" => $data['address1'].' '.$data['address2'],
-      "city_name" => $data['city'],
-      "state" => $data['state'],
-      "zip" => $data['zip'],
-      "country" => "USA",
-      "phone_number" => $data['phone'],
-      "fax_number" => $data['fax'],
-      "tollfree_number" => $data['tollfree'],
-      "payment" => "",
-      "hours_open" => "",
-      "email" => $data['email'],
-      "website" => $data['website'],
-      "api_ts" => time(),
-      "api_user_identifier" => md5($data['email']));
-     
-    $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
-    curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
-    return $this->response_parse($response);    
-  }  
-  
-  ///api/reputation_management/unregister_location
-  function repman_unregister_location($location_id) {
-    $operation = "/api/search/locations?";
-    $query = array(
-      "api_key" => $this->api_key,
-      "location_id" => $location_id,
-      "api_ts" => time(),
-      "api_user_identifier" => md5($email));
-     
-    $query_string = $this->generate_query($query).$this->generate_sig($query); 
-    $curl_url = $this->base_url.$operation.$query_string;
-    
-    curl_setopt($this->ch, CURLOPT_URL, $curl_url); 
-    $response = curl_exec($this->ch);  
-    
+    $response = curl_exec($this->ch);    
     return $this->response_parse($response);
   }
   
-  
-  //Protected utility functions
   protected function generate_query($data) {
     $query = "";
     foreach($data as $key => $value) {
-      $query .= $key."=".$value."&";
+      $query .= $key."=".str_replace (" ", "%20", $value)."&";
     }
     return $query;
   }
@@ -179,17 +141,8 @@ class YellowbotModel extends Model {
     return "api_sig=".hash_hmac("sha256", $sig, $this->api_secret);
   }
   
-  protected function encode_array($ary) {
-    foreach($ary as $key => $value) {
-      $ary[$key] = urlencode($value);
-    }
-    return $ary;
-  }
-  
   protected function response_parse($response) {
     return json_decode($response, TRUE);
-  }
-  
-  
+  }  
 }
 ?>
