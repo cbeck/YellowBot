005dc0e50f5d4ef7d68f8fa1fb4ca54292793980
JCBarry <jay@jcbarry.com>
2010-11-04 21:43:51 -0400

    Updated config and removed unused business model.

--- web/config/yellowbot.php
+++ web/config/yellowbot.php
@@ -3,3 +3,8 @@
 $config['api_key'] = "59667cf953f4c37f0d760c003a287972";
 //Yellowbot API Secret
 $config['api_secret'] = "f89d48e031440891";
+
+//Payment Processing Service URL
+$config['payment_service_url'] =  "https://qa.ubl.org/Payment.svc";
+//Payment Processing Service WSDL
+$config['payment_service_wsdl'] = "https://qa.ubl.org/Payment.svc?wsdl";
