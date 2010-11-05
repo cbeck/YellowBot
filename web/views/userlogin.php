53d2691f0ff5941cea503a3b9f15aea1a89af3b0
cbeck <chris@netphase.com>
2010-11-05 01:30:00 -0400

    added some lib to format forms and cleaned a couple of pages up after helping debug the SOAP calls

--- web/views/userlogin.php
+++ web/views/userlogin.php
@@ -7,17 +7,35 @@
   <div id="canvas" class="container_12">
     <?php include("includes/header.inc"); ?>
     <div id="main" class="container_12">
+      <div class="grid_6 suffix_6">
+      <h3>Login</h3>
       <?= validation_errors(); ?>
       <h5><?= $this->session->flashdata('success'); ?></h5>
-      <?= form_open('user/login'); ?>
-        <h5>Email</h5>
-        <input type="text" name="email" value="<?= set_value('email'); ?>" />   
-        <h5>Password</h5>
-        <input type="password" name="password" value="" />
+      <?php $attributes = array('class' => 'jqtransform', 'id' => 'login_form'); ?>
+      <?= form_open('user/login', $attributes); ?>
+      <fieldset>
+        <div class="rowElem">        
+            <label name="email">Email</label>
+            <input type="text" name="email" value="<?= set_value('email'); ?>" /> 
+        </div>
+        <div class="rowElem">
+            <label name="password">Password</label>
+            <input type="password" name="password" value="" />
+        </div>        
+      </fieldset>
+      <div class="rowElem">
         <input type="submit" value="Submit" />
+      </div>
       <?= form_close(); ?>
+      </div>
     </div>
     <?php include("includes/footer.inc"); ?>  
   </div>
+  <script type="text/javascript">
+  $(function() {
+      //find all form with class jqtransform and apply the plugin
+      $("form.jqtransform").jqTransform();
+  });
+  </script>
 </body>
 </html>
--- web/views/usersignup.php
+++ web/views/usersignup.php
@@ -8,32 +8,82 @@
     <?php include("includes/header.inc"); ?>
     <div id="main" class="container_12">
       <h2>Sign up</h2>
+      <div class="grid_6 suffix_6">
       <?= validation_errors(); ?>
-      <?= form_open('user/signup'); ?>
-        <label>Name:</label><input type="text" name="name" value="<?= set_value('name'); ?>" /><br />   
-        <label>Email:</label><input type="text" name="email" value="<?= set_value('email'); ?>" /><br />  
-        <label>Password:</label><input type="password" name="password" value="" /><br />
-        <label>Password Confirmation:</label><input type="password" name="passwordconf" value="" /><br />
-        <hr />
-        <label>Business Name:</label><input type="text" name="business_name" value="<?= set_value('business_name'); ?>" /><br />
-        <label>Address:</label><input type="text" name="address1" value="<?= set_value('address1'); ?>" /><br />  
-        <label>Address:</label><input type="text" name="address2" value="<?= set_value('address2'); ?>" /><br />  
-        <label>City:</label><input type="text" name="city" value="<?= set_value('city'); ?>" /><br />  
-        <label>State:</label><input type="text" name="state" value="<?= set_value('state'); ?>" /><br />  
-        <label>Zip:</label><input type="text" name="zip" value="<?= set_value('zip'); ?>" /><br />  
-        <label>Phone:</label><input type="text" name="phone" value="<?= set_value('phone'); ?>" /><br /> 
-        <hr />
-        <label>Name on Card:</label><input type="text" name="cc_name" value="<?= set_value('cc_name'); ?>" /><br /> 
-        <label>Type:</label><input type="text" name="cc_type" value="<?= set_value('cc_type'); ?>" /><br />
-        <label>Credit Card #:</label><input type="text" name="cc_num" value="<?= set_value('cc_num'); ?>" /><br />          
-        <label>Expiration Month:</label><input type="text" name="cc_exp_month" value="<?= set_value('cc_exp_month'); ?>" /><br /> 
-        <label>Expiration Year:</label><input type="text" name="cc_exp_year" value="<?= set_value('cc_exp_year'); ?>" /><br /> 
-        <label>CVV:</label><input type="text" name="cc_cvv" value="" /><br />  
-        
+      <?php $attributes = array('class' => 'jqtransform', 'id' => 'signup_form'); ?>
+      <?= form_open('user/signup', $attributes); ?>
+      <fieldset>
+        <legend>About You</legend>
+        <div class="rowElem">
+        <label>Name</label><input type="text" name="name" value="<?= set_value('name'); ?>" />
+        </div> 
+        <div class="rowElem">
+        <label>Email</label><input type="text" name="email" value="<?= set_value('email'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Password</label><input type="password" name="password" value="" />
+        </div>
+        <div class="rowElem">
+        <label>Password Confirmation</label><input type="password" name="passwordconf" value="" />
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>About Your Business</legend>
+        <div class="rowElem">
+        <label>Business Name:</label><input type="text" name="business_name" value="<?= set_value('business_name'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Address:</label><input type="text" name="address1" value="<?= set_value('address1'); ?>" />
+        </div>  
+        <div class="rowElem">
+        <label>Address:</label><input type="text" name="address2" value="<?= set_value('address2'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>City:</label><input type="text" name="city" value="<?= set_value('city'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>State:</label><input type="text" name="state" value="<?= set_value('state'); ?>" /> 
+        </div>
+        <div class="rowElem">
+        <label>Zip:</label><input type="text" name="zip" value="<?= set_value('zip'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Phone:</label><input type="text" name="phone" value="<?= set_value('phone'); ?>" />
+        </div>
+       </fieldset>
+       <fieldset>
+         <legend>Payment Information</legend>
+         <div class="rowElem">
+        <label>Name on Card:</label><input type="text" name="cc_name" value="<?= set_value('cc_name'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Type:</label><input type="text" name="cc_type" value="<?= set_value('cc_type'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Credit Card #:</label><input type="text" name="cc_num" value="<?= set_value('cc_num'); ?>" />
+        </div>   
+        <div class="rowElem">    
+        <label>Expiration Month:</label><input type="text" name="cc_exp_month" value="<?= set_value('cc_exp_month'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>Expiration Year:</label><input type="text" name="cc_exp_year" value="<?= set_value('cc_exp_year'); ?>" />
+        </div>
+        <div class="rowElem">
+        <label>CVV:</label><input type="text" name="cc_cvv" value="" />
+        </div>
+       </fieldset> 
         <input type="submit" value="Submit" />
       <?= form_close(); ?>
+      </div>
+      <div class="clear"></div>
     </div>
     <?php include("includes/footer.inc"); ?>  
   </div>
+  <script type="text/javascript">
+  $(function() {
+      //find all form with class jqtransform and apply the plugin
+      $("form.jqtransform").jqTransform();
+  });
+  </script>
 </body>
 </html>
