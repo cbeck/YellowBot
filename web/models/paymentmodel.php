0d47095f94d3014c05862d18a7b3b55ec9d971c5
JCBarry <jay@jcbarry.com>
2010-11-05 21:10:48 -0400

    Completed PaymentModel based on the expected results of the payment service.

---
+++ web/models/paymentmodel.php
@@ -0,0 +1,39 @@
+<?php
+class PaymentModel extends Model {
+  var $soap_client;
+  var $wsdl;
+  var $soap_url;
+  
+  
+  function PaymentModel() {
+    parent::Model();
+    $this->wsdl = $this->config->item('payment_service_wsdl');
+    $this->soap_url = $this->config->item('payment_service_url');  
+    $this->soap_client = new SoapClient($this->wsdl);
+  }
+  
+  function process_payment($data) {
+    $result = $this->soap_client->ProcessPayment($data);
+    return $this->validate_response($result); 
+  }
+  
+  protected function generate_request($ary) {
+    $request = "<CcPaymentRequest>";  
+    foreach($ary as $key => $value) {
+      $request .= "<".$key.">".$value."</".$key.">";
+    }
+    $request .= "</CcPaymentRequest>";
+    return $request;
+  }
+  
+  protected function validate_response($response) {
+    $xmlobject = new SimpleXMLElement($response->ProcessPaymentResult);
+    if($xmlobject->Status == "Unsuccessful") {
+      return false;
+    } else {
+      //Get payment number
+      return true;
+    }
+  }
+}
+?>
