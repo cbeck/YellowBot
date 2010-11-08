16b9fc22838f523ed43997b984810593de50dc72
cbeck <chris@netphase.com>
2010-11-08 00:24:29 -0500

    home page blocks

--- web/views/static/home.php
+++ web/views/static/home.php
@@ -14,9 +14,41 @@
       <div class="grid_6"></div>
     </div>
     <div id="main" class="container_12">
-      <div class="content_box"></div>
-      <div class="content_box"></div>
-      <div class="content_box"></div>
+      <div class="grid_8 alpha" id="home_content">
+        <div class="content_box">
+          <h2>Why do I need to manage my reputation?</h2>
+          The internet and social networks have created an explosion of amateur reviewers and critics, giving equal voice to a wide variety of opinion that ranges from balanced appreciation to wild attacks and prejudiced comments from disgruntled users and competitors.
+          Every day there are new sites that publish such commentaries - it is impossible to keep track of them separately and run a business at the same time.
+        </div>
+        <div class="content_box">
+          <h2>One tool to monitor all.</h2>
+          LocationMonitor.com provides an easy-to-use platform to monitor all reviews, mentions, ratings and listings about your business. With links directly to each site you can respond instantly to correct any false information or join customer discussions. 
+        </div>
+        <div class="content_box">
+          <h2>Don't be left out of the conversation.</h2>
+          People are talking - across the Web, blogs, social networks, review sites and industry portals, so being on top of the discussion when it relates to your business makes it easy to manage your reputation. Knowing is the first step to correcting.
+        </div>
+        <h5><a href="/user/signup">Get Started Now ></a></h5>
+      </div>
+      <div class="grid_4 omega">
+        <div class="home_feature_block">
+          <h3>Alert</h3>
+          <p>Be alerted to negative comments so you can take immediate action and respond.</p>
+        </div>
+        <div class="home_feature_block">
+          <h3>Protect</h3>
+          <p>Protect your Brand and company name from misuse or abuse.</p>
+        </div>
+        <div class="home_feature_block">
+          <h3>Connect</h3>
+          <p>Make sure customers are able to connect with accurate phone and address information in listings.</p>
+        </div>
+        <div class="home_feature_block">
+          <h3>Compete</h3>
+          <p>See how the sentiment about your business compares with your competitors.</p>
+        </div>
+      </div>
+      <div class="clear"></div>
     </div>
     <?php include(dirname(__FILE__)."/../includes/footer.inc"); ?>  
   </div>
