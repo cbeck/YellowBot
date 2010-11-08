cbf9db9961244b0242dcefd1245f328fe8e7e6d7
cbeck <chris@netphase.com>
2010-11-07 23:33:07 -0500

    features updated text

--- web/views/static/features.php
+++ web/views/static/features.php
@@ -15,10 +15,11 @@
           <li><a href="#reviews">Reviews</a></li>
           <li><a href="#mentions">Mentions</a></li>
           <li><a href="#competitors">Competitors</a></li>
+          <li><a href="#alerts">Alerts &amp; Reports</a></li>
         </ul>
         <div id="overview" class="tabs">
           <p>Get an executive summary of your web presence and reputation. Use this page as an entry point to quickly navigate to other sections.</p>
-            <img src="../assets/images/Locationmonitor_Overview.jpg" style="float:left;"/>         
+            <img src="../assets/images/Locationmonitor_Overview.jpg"/>         
             <ul>
               <li>See at a glance how many listings exist, and how many may have bad data. Find out if the business has missing or duplicated listings on top-tier sites.</li>
               <li>Gauge the overall ratings and popularity of a business on review sites.</li>
@@ -30,34 +31,83 @@
         <div id="visibility" class="tabs"> 
           <p>There's a lot of bad data out there. Manage your web presence and enhance your search engine optimization by finding and correcting inaccurate listings across the web.</p>
           <img src="../assets/images/Locationmonitor_Visibility.jpg"/>
+          <ul>
+            <li>Filter listings by type of site: local search, blogs, etc.</li>
+            <li>Missing and incorrect data is automatically identified and flagged.</li>
+            <li>A direct link takes merchants to the listing site so they can make corrections.</li>
+          </ul>
+          <div class="clear"></div> 
         </div>
         <div id="reviews" class="tabs">  
           <p>Save time by viewing reviews from all across the web. Plus, you get the tools to share and respond to those reviews – all in one place.</p>
           <img src="../assets/images/Locationmonitor_Reviews.jpg"/>
+          <ul>
+            <li>Merchants can click and drag on the central ratings index to see ratings for a specific period of time.</li>
+            <li>No need to read every review -- the tool automatically extracts and analyzes user sentiments to identify a business's strengths and weaknesses.</li>
+            <li>Easy-to-use search, with the option to filter reviews by keyword, rating, and website.</li>                                    
+            <li>Charts and graphs give a quick visual gauge of the business's overall ratings.</li>                                            
+            <li>Merchants can drill down to read individual reviews from social networks (Twitter, Facebook), local search sites (Yelp, CityG  rid), travel sites (TripAdvisor), blogs, and more.</li>
+            <li>Merchants are just a click away from sharing reviews on their own Twitter, Facebook, and other social network accounts.</li>   
+          </ul>
+          <div class="clear"></div>
         </div>
         <div id="mentions" class="tabs">
           <p>Reviews are not the only way people talk about a business - there is also tweeting, sharing photos, and "checking in". Manage information overload with a single page to quickly see everything that is being said about your business.</p>
           <img src="../assets/images/Locationmonitor_Mentions.jpg"/>
+          <ul>
+            <li>Easy-to-use search functionality, with the option to filter mentions by site</li>
+            <li>Sentiment analysis of the mentions shows at a glance whether people feel generally positive or negative about a business.</li>
+            <li>Merchants see a single stream of all their mentions: tweets from Twitter, check-ins on Foursquare or Gowalla, photos on Flickr, videos on YouTube, and more.</li>
+            <li>Merchants can integrate the tool with their Twitter account, so it's easy to share and respond to comments all in one place.</li>
+          </ul>
+          <div class="clear"></div>
         </div>
         <div id="competitors" class="tabs">
           <p>Put reviews and mentions into perspective: see how your online reputation compares to your  direct competitors.</p>
           <img src="../assets/images/Locationmonitor_Competitors.jpg"/>
+          <ul>
+            <li>Merchants see how their ratings compare with those of their competitors.</li>
+            <li>Direct comparisons motivate merchants to use your tools and maintain a competitive edge online.</li>
+            <li>Merchants can choose any business they want as a competitor, or have the system suggest competitors.</li>
+          </ul>          
+          <div class="clear"></div>
+        </div>
+        <div id="alerts" class="tabs">
+          <p>Put reviews and mentions into perspective: see how your online reputation compares to your  direct competitors.</p>
           <img src="../assets/images/Locationmonitor_Competitors_2.jpg"/>
+          <ul>
+            <li>Stay at the top of your customers' minds with regular reports that remind users of your product's value.</li>
+            <li>Merchants who want to aggressively manage their reputations can be updated in real time whenever a new review, listing, or mention is found.</li>
+            <li>Customize the available reports and alerts depending on your implementation.</li>
+          </ul>
+          <div class="clear"></div>
         </div>
       </div>
       
       <h3>What are the benefits of Reputation and Presence Management System?</h3>
       <ul>
-        <li><p>Alert/Be alerted to negative comments so you can take immediate action and respond.</p></li>
-        <li><p>Protect/Protect your Brand and company name from misuse or abuse.</p></li>
-        <li><p>Connect/Make sure customers are able to connect with accurate phone and address information in listings.</p></li>
-        <li><p>Compete/ See how the sentiment about your business compares with your competitors.</p></li>
+        <li>Alert/Be alerted to negative comments so you can take immediate action and respond.</li>
+        <li>Protect/Protect your Brand and company name from misuse or abuse.</li>
+        <li>Connect/Make sure customers are able to connect with accurate phone and address information in listings.</li>
+        <li>Compete/ See how the sentiment about your business compares with your competitors.</li>
       </ul>
       
-      <h3>What is the Reputation and Presence Management?</h3>
+      <h3>What is Reputation and Presence Management?</h3>
       <p>LocationMonitor.com helps you monitor your online reputation by finding everything that is said about your business in reviews and on social networking sites and displaying it in one easy-to-use dashboard.</p>
       <p>There's also a lot of bad local information on the web -- incomplete, inaccurate, or duplicative listings that can make it difficult for you to connect with your customers. LocationMonitor.com's reputation management system finds that listing information and makes it easy for you to correct it.</p>
       <p>Locationmonitor.com provides a single location where you can not only understand what's being said about you online, but also actively manage your information and participate in conversations that are happening across the Web. Don't let the buzz turn into a buzz-saw!</p>
+      
+      <h3>Reputation and Presence Management System: Back End</h3>
+      <p>Know your data is up-to-date, thorough, and the highest quality.</p>
+      <ul>
+        <li>Rapid deployment starts our crawlers looking for your merchants immediately</li>
+        <li>Crawlers aggregate listings, reviews, and mentions from hundreds and hundreds of websites</li>
+        <li>Proprietary data matching technology produces accurate and relevant results</li>
+        <li>Deep crawling finds information even on sites with just a few reviews</li>
+        <li>Real-time updates mean data is continuously discovered, improved, and merged</li>
+      </ul>
+
+      With Location Monitror's fast crawlers, your listings are matched, merged, and maintained on a continuous basis.
     </div>
     <?php include(dirname(__FILE__)."/../includes/footer.inc"); ?>  
   </div>
