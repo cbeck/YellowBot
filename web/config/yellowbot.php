d5e64bf8f04fb499a01caec43e232b1d15f9746c
JCBarry <jay@jcbarry.com>
2010-11-07 20:35:14 -0500

    Updated payment URL and added default payment frequency.

--- web/config/yellowbot.php
+++ web/config/yellowbot.php
@@ -1,13 +1,14 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 //Yellowbot API Key
-$config['api_key'] = "59667cf953f4c37f0d760c003a287972";
+$config['api_key'] = "c7cbf3afd2cedd3c69cd091a8f2e7347";
 //Yellowbot API Secret
-$config['api_secret'] = "f89d48e031440891";
+$config['api_secret'] = "66f4cdbbd65cb13a";
 
 //Payment Processing Service URL
-$config['payment_service_url'] =  "https://qa.ubl.org/Payment.svc";
-//Payment Processing Service WSDL
-$config['payment_service_wsdl'] = "https://qa.ubl.org/Payment.svc?wsdl";
+$config['payment_service_url'] =  "https://qa.ubl.org/payment.svc";
 
 //Default Price
 $config['default_price'] = '1.99';
+
+//Default Frequency
+$config['default_frequency'] = "12";
